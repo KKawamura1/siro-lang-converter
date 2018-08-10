63a2f04014ed71bc32e2052c8d92db0282cdedbc
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-08-10 20:12:43 +0900

    update birthday program

--- char_to_brain.py
+++ char_to_brain.py
@@ -17,28 +17,29 @@ def num_to_10bases(num: int, max_num_digits: int = 5) -> Sequence[int]:
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('string', type=str)
+    parser.add_argument('strings_file', type=argparse.FileType())
     params = parser.parse_args()
-    string = params.string
-    if len(string) == 1:
-        chars = [string]
-    else:
-        chars = list(string)
+    strings_file = params.strings_file
+    for string in strings_file.readlines():
+        if len(string) == 1:
+            chars = [string]
+        else:
+            chars = list(string)
 
-    ords = chars_to_ords(chars)
-    num_ords = len(ords)
-    ords_with_10bases = [num_to_10bases(num) for num in ords]
-    str1 = '[-]>' * (4 + num_ords) + '[-]' + '<' * (4 + num_ords)
-    str2 = (
-        f'++++++++++[>++++++++++[>++++++++++[>++++++++++[> '
-        f'{ ">".join(["+" * n for n in [bases10[0] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } <-]'
-        f'> { ">".join(["+" * n for n in [bases10[1] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } < <-]'
-        f'>> { ">".join(["+" * n for n in [bases10[2] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } << <-]'
-        f'>>> { ">".join(["+" * n for n in [bases10[3] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } <<< <-]'
-        f'>>>> { ".>".join(["+" * n for n in [bases10[4] for bases10 in ords_with_10bases]]) } . { "<" * (num_ords - 1) } <<<<'
-    )
-    print(str1)
-    print(str2)
+        ords = chars_to_ords(chars)
+        num_ords = len(ords)
+        ords_with_10bases = [num_to_10bases(num) for num in ords]
+        str1 = '[-]>' * (4 + num_ords) + '[-]' + '<' * (4 + num_ords)
+        str2 = (
+            f'++++++++++[>++++++++++[>++++++++++[>++++++++++[> '
+            f'{ ">".join(["+" * n for n in [bases10[0] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } <-]'
+            f'> { ">".join(["+" * n for n in [bases10[1] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } < <-]'
+            f'>> { ">".join(["+" * n for n in [bases10[2] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } << <-]'
+            f'>>> { ">".join(["+" * n for n in [bases10[3] for bases10 in ords_with_10bases]]) } { "<" * (num_ords - 1) } <<< <-]'
+            f'>>>> { ".>".join(["+" * n for n in [bases10[4] for bases10 in ords_with_10bases]]) } . { "<" * (num_ords - 1) } <<<<'
+        )
+        print(str1)
+        print(str2)
 
 
 if __name__ == '__main__':
